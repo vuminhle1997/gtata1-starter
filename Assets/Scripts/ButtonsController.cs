535e232b2ab6be830cb02d65fa30983171382ac4
Minh Le <vuminhle@outlook.com>
2021-05-10 10:39:15 +0200

    First UI

---
+++ Assets/Scripts/ButtonsController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonsController : MonoBehaviour
+{
+    public void ButtonPressed(int type)
+    {
+        switch (type)
+        {
+            case 1:
+                Debug.Log("Pause/Resume pressed");
+                break;
+            case 2:
+                Debug.Log("Menu pressed");
+                break;
+            default:
+                throw new NotImplementedException("No button logic implemented");
+        }
+    }
+
+    public void Foo(int i)
+    {
+        Debug.Log(i);
+    }
+}
